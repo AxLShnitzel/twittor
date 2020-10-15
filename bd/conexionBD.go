670278ec48b61f8e8f7bb6b4fd20e9655ce98892
AxL <andres.lopez.becerra@gmail.com>
2020-10-15 15:09:16 -0500

    Registro de usuario

---
+++ bd/conexionBD.go
@@ -0,0 +1,38 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// MongoCN es el objeto de conexion a la BD
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://axl:10killwithme@cluster0.bhaoi.mongodb.net/<dbname>?retryWrites=true&w=majority")
+
+// ConectarBD conecta a la BD de mongo
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexion exitosa con la BD")
+	return client
+}
+
+// ChequeoConnection chequea la conexion
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/AxLShnitzel/twittor/bd"
+	"github.com/AxLShnitzel/twittor/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexion a la BD")
+		handlers.Manejadores()
+	}
 }
